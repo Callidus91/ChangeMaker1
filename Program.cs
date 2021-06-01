fa40c8d478c72e2ee8f23152eb05e553faf9947b
trgarner <trgarner91@gmail.com>
2021-05-31 22:42:53 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ChangeMaker1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int change;
+
+            Console.Write("Enter your change: ");
+            change = Convert.ToInt32(Console.ReadLine());
+            
+
+            int quarters = change / 25;
+            change %= 25;
+            int dimes = change / 10;
+            change %= 10;
+            int nickels = change / 5;
+            int pennies = change - nickels * 5;
+            Console.WriteLine("quarters: " + quarters);
+            Console.WriteLine("dimes: " + dimes);
+            Console.WriteLine("nickels: " + nickels);
+            Console.WriteLine("pennies: " + pennies);
+
+            if (change > 99)
+            {
+                Console.WriteLine("No can do");
+                
+            }
+            
+            
+            Console.Write("Do you want more?");
+            string ans = Console.ReadLine();
+
+            Console.ReadLine();
+        }
+
+    }
+}
